4f8e005d6b35c62cfdf8d67df7f4b076547f6755
DESKTOP-KBCK0AM\Danada <thesinhound@gmail.com>
2018-01-15 12:43:40 -0800

    Final Section 01 Commit

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -2,6 +2,44 @@
 
 #include "PuzzlePlatformsGameInstance.h"
 
+#include "Engine/Engine.h"
 
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
 
+}
 
+void UPuzzlePlatformsGameInstance::Init()
+{
+	
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/PuzzlePlatforms/Maps/TestMap?listen");
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString & Address)
+{
+	UEngine* Engine = GetEngine();
+
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -13,8 +13,15 @@ UCLASS()
 class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer);
+
+	virtual void Init();
 	
-	
-	
-	
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& Address);
 };
